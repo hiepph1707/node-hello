63e89b4491a219e9982ac3e9d49f62c62b417a8f
hiepph1707 <57936136+hiepph1707@users.noreply.github.com>
2021-05-19 17:29:46 +0700

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker login -u hiepph1707 -p $PASS
-/usr/local/bin/docker-compose -f docker-compose-deploy.yml up -d
+cd jenkins/deploy/ && /usr/local/bin/docker-compose -f docker-compose-deploy.yml up -d
